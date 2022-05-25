ac04d6b99291da8b6f9c1512ad002c2c637e3b33
Marcin Witkowski <mwitkowski@antmicro.com>
2022-05-25 11:33:08 +0200

    [#33246] Pass Renode parameters to the Renode GitHub action

--- src/run_renode_test.sh
+++ src/run_renode_test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Convert string into array
+renode_arguments=( $(xargs -n1 printf '%s\n' <<< "$RENODE_ARGUMENTS") )
+
 # Install renode-run script
 pip install -q git+https://github.com/antmicro/renode-run
 
@@ -12,7 +15,7 @@ if [ -z "$TESTS_TO_RUN" ]
 then
     echo "No tests provided, renode-run artifacts are installed to $RENODE_RUN_DIR"
 else
-    renode-run -a $RENODE_RUN_DIR test -- $TESTS_TO_RUN
+    renode-run -a $RENODE_RUN_DIR test -- "${renode_arguments[@]}" $TESTS_TO_RUN
 fi
 
 echo "::remove-matcher owner=test-in-renode::"
