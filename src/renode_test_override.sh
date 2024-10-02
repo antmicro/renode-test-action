dadb56a4fce5d269561a397a3028c1891525b624
Andrzej Drabarek <adrabarek@antmicro.com>
2024-10-02 11:02:03 +0200

    [DEV] Simplify renode_test_override.sh

--- src/renode_test_override.sh
+++ src/renode_test_override.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-ROOT="$(cd $(dirname $(readlink -f $0 2>/dev/null || echo $0)); cd ..; echo $PWD)"
-py -3 "$ROOT/tests/run_tests.py" --css-file "$ROOT/tests/robot.css" --exclude "skip_windows" --robot-framework-remote-server-full-directory  "$ROOT/bin" -r "$PWD" "$@"
+py -3 "$RENODE_ROOT/tests/run_tests.py" --css-file "$RENODE_ROOT/lib/resources/styles/robot.css" --exclude "skip_windows" --robot-framework-remote-server-full-directory  "$RENODE_ROOT/output/bin/Release" -r "$PWD" "$@"
